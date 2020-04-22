4d91ad81cf22f14c59c637a728d4923f28109c36
Dan Milne <d@nmilne.com>
2020-04-22 14:07:24 +1000

    Dump byebug history file

---
+++ test/fixtures.rb
@@ -0,0 +1,90 @@
+TESTCASES= [
+  isbn: '9780316030571',
+  data: {
+    "ISBN:9780316030571": {
+        "authors": [
+            {
+                "name": "Iain M. Banks",
+                "url": "https://openlibrary.org/authors/OL6924809A/Iain_M._Banks"
+            }
+        ],
+        "cover": {
+            "large": "https://covers.openlibrary.org/b/id/2379038-L.jpg",
+            "medium": "https://covers.openlibrary.org/b/id/2379038-M.jpg",
+            "small": "https://covers.openlibrary.org/b/id/2379038-S.jpg"
+        },
+        "ebooks": [
+            {
+                "availability": "borrow",
+                "borrow_url": "https://openlibrary.org/books/OL10426208M/Use_of_Weapons/borrow",
+                "checkedout": false,
+                "formats": {},
+                "preview_url": "https://archive.org/details/useofweapons00bank_0"
+            }
+        ],
+        "identifiers": {
+            "goodreads": [
+                "3395563"
+            ],
+            "isbn_10": [
+                "0316030570"
+            ],
+            "isbn_13": [
+                "9780316030571"
+            ],
+            "librarything": [
+                "141"
+            ],
+            "openlibrary": [
+                "OL10426208M"
+            ]
+        },
+        "key": "/books/OL10426208M",
+        "number_of_pages": 480,
+        "publish_date": "July 28, 2008",
+        "publishers": [
+            {
+                "name": "Orbit"
+            }
+        ],
+        "subject_people": [
+            {
+                "name": "Cheradenine Zakalwe",
+                "url": "https://openlibrary.org/subjects/person:cheradenine_zakalwe"
+            },
+            {
+                "name": "Diziet Sma",
+                "url": "https://openlibrary.org/subjects/person:diziet_sma"
+            },
+            {
+                "name": "Skaffen-Amtiskaw",
+                "url": "https://openlibrary.org/subjects/person:skaffen-amtiskaw"
+            }
+        ],
+        "subjects": [
+            {
+                "name": "Fiction",
+                "url": "https://openlibrary.org/subjects/fiction"
+            },
+            {
+                "name": "Science Fiction",
+                "url": "https://openlibrary.org/subjects/science_fiction"
+            },
+            {
+                "name": "Science Fiction & Fantasy",
+                "url": "https://openlibrary.org/subjects/science_fiction_&_fantasy"
+            },
+            {
+                "name": "Long Now Manual for Civilization",
+                "url": "https://openlibrary.org/subjects/long_now_manual_for_civilization"
+            },
+            {
+                "name": "Space warfare",
+                "url": "https://openlibrary.org/subjects/space_warfare"
+            }
+        ],
+        "title": "Use of Weapons",
+        "url": "https://openlibrary.org/books/OL10426208M/Use_of_Weapons"
+    }
+  }
+]
